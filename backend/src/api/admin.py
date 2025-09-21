0d141c35268aa4b07f22ac43a501593384d77c94
Roxamir <101136913+Roxamir@users.noreply.github.com>
2025-09-21 09:37:31 -0700

    feat(admin): Enhance model admin and add image previews
    Refactors the admin registration to use `ModelAdmin` classes with the `@admin.register` decorator for better organization and scalability.
    
    - Customizes the `list_display` for `Equipment` and `ConditionPhoto` to improve readability in the admin list view.
    - Adds an `image_preview` function to `ConditionPhotoAdmin` to display image thumbnails directly, making it easier to visually identify photos.

--- backend/src/api/admin.py
+++ backend/src/api/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Equipment, ConditionPhoto
+from django.utils.html import mark_safe
 
-# Register your models here.
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(ConditionPhoto)
+class ConditionPhotoAdmin(admin.ModelAdmin):
+    list_display = ('equipment', 'image_preview', 'photo_location', 'timestamp', 'contract_identifier')
+    list_filter = ('equipment', 'photo_location', 'timestamp')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="150" height="auto" />')
+        return "No Image"
+    
+    image_preview.short_description = 'Image Preview'
