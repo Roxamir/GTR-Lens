e6b48abd2f4e2ebd70a2cc2866dec15ebdb99684
Roxamir <101136913+Roxamir@users.noreply.github.com>
2025-11-05 12:31:15 -0800

    fix(frontend): fix api base URL

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-const API_BASE_URL =
+export const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/";
 
 const useAuth = () => {
